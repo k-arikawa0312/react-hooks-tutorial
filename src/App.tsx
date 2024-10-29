f5b3d9bc3edae79f39969a6d68e932563d7b65b5
k-arikawa0312 <s1F102402710@iniad.org>
2024-10-29 16:54:05 +0900

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import './App.css'
 import profileContext from './main'
 
 function App() {
   const [count,setCount]=useState(0)
   const profile = useContext(profileContext)
+  const ref=useRef()
 
     const handleClick=()=>{
       setCount(count+1)
@@ -13,6 +14,10 @@ function App() {
     useEffect(()=>{
       console.log("hello")
     },[count])
+
+    const handleRef=()=>{
+      console.log(ref)
+    }
 
   return (
     <>
@@ -24,6 +29,10 @@ function App() {
       <h1>useContext</h1>
       <p>{profile.name}</p>
       <p>{profile.age}</p>
+      <hr></hr>
+      <h1>useRef</h1>
+      <input type='text' ref={ref}></input>
+      <button onClick={handleRef}>useRef</button>
     </>
   )
 }
