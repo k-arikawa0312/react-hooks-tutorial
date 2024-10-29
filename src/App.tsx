258c36324250f49abd9daf99ef5214af4716c32b
k-arikawa0312 <s1F102402710@iniad.org>
2024-10-29 16:50:01 +0900

    useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './App.css'
+import profileContext from './main'
 
 function App() {
   const [count,setCount]=useState(0)
+  const profile = useContext(profileContext)
 
     const handleClick=()=>{
       setCount(count+1)
@@ -10,13 +12,18 @@ function App() {
 
     useEffect(()=>{
       console.log("hello")
-    })
+    },[count])
 
   return (
     <>
       <h1>useState,useEffect</h1>
       <button onClick={handleClick}>+</button>
       <h1>{count}</h1>
+
+      <hr></hr>
+      <h1>useContext</h1>
+      <p>{profile.name}</p>
+      <p>{profile.age}</p>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+import { createContext, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const profile={
+  name:"k-arikawa",
+  age:24,
+}
+
+const profileContext= createContext(profile)
+
 createRoot(document.getElementById('root')!).render(
+  <profileContext.Provider value={profile}>
   <StrictMode>
     <App />
   </StrictMode>,
+  </profileContext.Provider>
 )
+
+export default profileContext
