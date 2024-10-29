49b1161b753428655f4c4a8697a7f10b2f23e92c
k-arikawa0312 <s1F102402710@iniad.org>
2024-10-29 17:05:41 +0900

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useReducer, useRef, useState } from 'react'
 import './App.css'
 import profileContext from './main'
+
+const reducer = (state: number, action: { type: string }) => {
+  switch(action.type){
+    case "increment":
+      return state+1
+    case "decrement":
+      return state-1
+    default:
+      return state
+  }
+}
 
 function App() {
   const [count,setCount]=useState(0)
   const profile = useContext(profileContext)
-  const ref=useRef()
+  const ref = useRef<HTMLInputElement>(null)
+  const [state,dispatch] = useReducer(reducer,0)
 
     const handleClick=()=>{
       setCount(count+1)
@@ -17,6 +29,8 @@ function App() {
 
     const handleRef=()=>{
       console.log(ref)
+      console.log(ref.current?.name)
+      console.log(ref.current?.value)
     }
 
   return (
@@ -29,10 +43,17 @@ function App() {
       <h1>useContext</h1>
       <p>{profile.name}</p>
       <p>{profile.age}</p>
+
       <hr></hr>
       <h1>useRef</h1>
       <input type='text' ref={ref}></input>
       <button onClick={handleRef}>useRef</button>
+      
+      <hr></hr>
+      <h1>useReducer</h1>
+      <p>カウント：{state}</p>
+      <button onClick={()=>dispatch({type:"increment"})}>+</button>
+      <button onClick={()=>dispatch({type:"decrement"})}>-</button>
     </>
   )
 }
